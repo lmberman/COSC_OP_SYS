b8b3c0a73345988e3fd99581203b1120180d3eee
ceerie <65731083+ceerie@users.noreply.github.com>
2021-04-16 20:37:47 -0400

    Update TestScript1.sh

--- src/TestScript1.sh
+++ src/TestScript1.sh
@@ -1,4 +1,5 @@
-We need to add some script
-In this file here
-For loops or some shell code would work
-This is optional
+for (( counter=10; counter>0; counter-- ))
+do
+echo "A set of blocked processes each holding a resource and waiting to acquire a resource held by another process in the set."
+done
+printf "\n"
